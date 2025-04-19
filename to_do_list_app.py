7a24ce9d4980392f0ab127710dbc54f48762a7a1
anupama-g-bhat <anupamabhat6769@gmail.com>
2025-04-19 19:34:02 +0530

    Update to_do_list_app.py
    Checkbox support added

--- to_do_list_app.py
+++ to_do_list_app.py
@@ -1,16 +1,21 @@
 import tkinter as tk
 
+tasks = []
 
 def add_task():
     task = task_entry.get()   #takes input from the user
     if task :
-        listbox.insert(tk.END, f"• {task}")  # shows the list of tasks 
+        var = tk.BooleanVar()
+        chk =tk.Checkbutton(task_frame,text=task, variable=var,font =("Arial",12,"bold"))
+        chk.pack(anchor="w")
+        tasks.append((chk,var))
         task_entry.delete(0,tk.END)
 
 def remove_task():
-    selected_task =listbox.curselection() # get selected item index
-    if selected_task :
-        listbox.delete(selected_task[0]) # deletes that item
+    for chk,var in tasks[:] :
+        if var.get():
+            chk.destroy()
+            tasks.remove((chk,var))
 
 #Create a main window
 root = tk.Tk()
@@ -22,19 +27,21 @@ root.geometry("500x450")
 title_label = tk.Label(root,text = "My To-Do List", font=("Arial",20,"bold"))
 title_label.pack(pady=20)
 
-listbox = tk.Listbox(root, width=40, height=10, font=("Arial", 12))
-listbox.pack(pady=20)
-
 task_entry = tk.Entry(root, width=30)
 task_entry.pack(pady=10)
 
-#craete a button
+# Frame to hold tasks (checkboxes)
+task_frame=tk.Frame(root)
+task_frame.pack(pady=10)
+
+#create a button
 button_frame =tk.Frame(root)
 button_frame.pack()
 
-tk.Button(button_frame,text="Add task",command=add_task,font=("Arial",14,"bold"), fg="yellow",bg="green").grid(row=0,column=0,padx=10)
-tk.Button(button_frame,text="Remove",command=remove_task,font=("Arial",14,"bold"), fg="yellow",bg="green").grid(row=0,column=1,padx=10)
+tk.Button(button_frame,text="Add Task",command=add_task,font=("Arial",14,"bold"), fg="yellow",bg="brown",bd =4).grid(row=0,column=0,padx=10)
+tk.Button(button_frame,text="Remove Task",command=remove_task,font=("Arial",14,"bold"), fg="yellow",bg="brown",bd =4).grid(row=0,column=1,padx=10)
 
 
 #start the main loop
 root.mainloop()
+
